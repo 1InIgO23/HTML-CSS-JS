5a6a5cb8e24f29427ffb665e36aeb259e6bed6e3
1InIgO23 <133599079+1InIgO23@users.noreply.github.com>
2023-07-12 10:15:05 +0200

    Add files via upload

---
+++ BreakingBad/Contact.js
@@ -0,0 +1,9 @@
+function obtenerValor() {
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var mail = document.getElementById("mail").value;
+    var number = document.getElementById("number").value;
+    var nickname = document.getElementById("nickname").value;
+
+    console.log(name + ", " + surname + ", " + mail + ", " + number + ", " + nickname);
+  }
